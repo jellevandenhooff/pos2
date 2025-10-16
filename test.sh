5df60930413ddad28785dbad6371b7cdb8d3e1d2
Jelle van den Hooff <jelle@vandenhooff.name>
2025-10-16 13:24:55 -0400

    dockering

--- test.sh
+++ test.sh
@@ -2,28 +2,6 @@
 
 set -ex
 
-nix build .#packages.x86_64-linux.loaderImage .#packages.x86_64-linux.innerappPlainImage
+docker build -t tunnel:testing -f Dockerfile.tunnel .
+docker run --rm -it -v "$(pwd)/tunnel/testing/client:/data" tunnel:testing /tunnel client /data
 
-LOADER="$(realpath result)"
-rm result
-INNER_APP="$(realpath result-1)"
-rm result-1
-
-echo "LOADER=$LOADER"
-echo "INNER_APP=$INNER_APP"
-
-LOADER_LOAD_RESULT="$(docker load -q < $LOADER)"
-LOADER_IMAGE="${LOADER_LOAD_RESULT#"Loaded image: "}"
-
-INNER_APP_LOAD_RESULT="$(docker load -q < $INNER_APP)"
-INNER_APP_IMAGE="${INNER_APP_LOAD_RESULT#"Loaded image: "}"
-
-docker tag $LOADER_IMAGE ghcr.io/jellevandenhooff/loader:experiment
-docker rmi $LOADER_IMAGE
-# docker push ghcr.io/jellevandenhooff/loader:experiment
-
-docker tag $INNER_APP_IMAGE ghcr.io/jellevandenhooff/innerapp:experiment
-docker rmi $INNER_APP_IMAGE
-docker push ghcr.io/jellevandenhooff/innerapp:experiment
-
-docker run --rm -it -e container=1 -v "$(pwd)/loaderdatavolume:/data" ghcr.io/jellevandenhooff/loader:experiment
