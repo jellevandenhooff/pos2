9dd7d312389e1434642e8c06de7f953a58b930a0
Jelle van den Hooff <jelle@vandenhooff.name>
2025-10-21 13:07:25 -0400

    the lock works???

--- wasi3app/src/lib.rs
+++ wasi3app/src/lib.rs
@@ -4,10 +4,22 @@ use wasip3::{wit_bindgen, wit_future, wit_stream};
 
 wasip3::http::proxy::export!(Example);
 
-struct Example;
+use lazy_static::lazy_static;
+
+struct Example {}
+
+lazy_static! {
+    static ref STATE: tokio::sync::Mutex<u64> = tokio::sync::Mutex::new(0u64);
+}
 
 impl wasip3::exports::http::handler::Guest for Example {
     async fn handle(_request: Request) -> Result<Response, ErrorCode> {
+        let value = {
+            let mut guard = STATE.lock().await;
+            *guard += 1;
+            *guard
+        };
+
         let (mut body_tx, body_rx) = wit_stream::new();
         let (body_result_tx, body_result_rx) = wit_future::new(|| Ok(None));
         let (response, _future_result) =
@@ -15,7 +27,9 @@ impl wasip3::exports::http::handler::Guest for Example {
         drop(body_result_tx);
 
         wit_bindgen::spawn(async move {
-            let remaining = body_tx.write_all(b"Hello, WASI!".to_vec()).await;
+            let remaining = body_tx
+                .write_all(format!("Hello, WASI! {}", value).into_bytes().to_vec())
+                .await;
             assert!(remaining.is_empty());
             wait_for(1_000_000_000).await; // 1s
 
